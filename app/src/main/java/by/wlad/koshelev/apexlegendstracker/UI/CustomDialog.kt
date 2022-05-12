27bf646e9781574708ce871cd7db0f915120511b
Punx <wlad178@mail.ru>
2022-05-12 10:13:02 +0300

    hotbugfix

--- app/src/main/java/by/wlad/koshelev/apexlegendstracker/UI/CustomDialog.kt
+++ app/src/main/java/by/wlad/koshelev/apexlegendstracker/UI/CustomDialog.kt
@@ -8,6 +8,8 @@ import by.wlad.koshelev.apexlegendstracker.R
 
 object CustomDialog {
 
+    hotbugfix
+
     fun alertDialog(context: Context, style: Int = R.style.AlertDialog) = AlertDialog.Builder(context, style)
 
 }
