f24c45997f2079e1f7a919163040a8eb029954c3
Punx <wlad178@mail.ru>
2022-05-13 08:47:53 +0300

    1111

--- app/src/main/java/by/wlad/koshelev/apexlegendstracker/UI/MainActiv/MainActivity.kt
+++ app/src/main/java/by/wlad/koshelev/apexlegendstracker/UI/MainActiv/MainActivity.kt
@@ -16,6 +16,8 @@ import java.util.*
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 
+        1111
+
         // настройка языка
         val lang: String = SharedPref(application).lang
         val locale = Locale(lang)
