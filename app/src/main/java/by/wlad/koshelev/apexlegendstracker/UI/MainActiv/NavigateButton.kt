432c844d4685241ef187c3a382819d30037c0b78
Punx <wlad178@mail.ru>
2022-05-13 09:56:09 +0300

    !DEV - .

--- app/src/main/java/by/wlad/koshelev/apexlegendstracker/UI/MainActiv/NavigateButton.kt
+++ app/src/main/java/by/wlad/koshelev/apexlegendstracker/UI/MainActiv/NavigateButton.kt
@@ -12,9 +12,6 @@ import kotlinx.android.synthetic.main.activity_main.*
  */
 object NavigateButton {
 
-
-
-    2222
     // контекст, который инициализируется и будет использоваться в других функциях
     private lateinit var app: MainActivity
 
